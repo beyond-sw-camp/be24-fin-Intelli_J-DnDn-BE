817d29a83017bcf6ef3ffec62a599e04ac9597e2
swoo <kokodd1234@gmail.com>
2026-04-16 17:49:36 +0900

    [CI/CD] cicd test #4

--- src/main/java/org/example/dndn/DnDnApplication.java
+++ src/main/java/org/example/dndn/DnDnApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DnDnApplication {
-	// cicd 3
+	// cicd 4
 	public static void main(String[] args) {
 		SpringApplication.run(DnDnApplication.class, args);
 	}
