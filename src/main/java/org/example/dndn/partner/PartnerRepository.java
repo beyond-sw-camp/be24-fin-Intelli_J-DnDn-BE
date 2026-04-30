d74e336093104b9181a82b46044b6a711d678c66
NahunE <myjsh6547@naver.com>
2026-04-30 15:52:18 +0900

    [Remove] 협력사 기능 폴더 삭제

--- src/main/java/org/example/dndn/partner/PartnerRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.dndn.partner;
-
-import org.example.dndn.partner.model.Partner;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PartnerRepository extends JpaRepository<Partner, Long> {
-}
