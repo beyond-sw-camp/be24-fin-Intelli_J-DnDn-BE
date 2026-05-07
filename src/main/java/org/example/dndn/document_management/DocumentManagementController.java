84b9bec5184562b238415ef93c3c6b4a2c5a4b59
mj <qjsalswn@gmail.com>
2026-05-07 11:33:09 +0900

    [Feat] 문서 업로드 백엔드 기능

--- src/main/java/org/example/dndn/document_management/DocumentManagementController.java
+++ src/main/java/org/example/dndn/document_management/DocumentManagementController.java
@@ -3,7 +3,9 @@ package org.example.dndn.document_management;
 import lombok.RequiredArgsConstructor;
 import org.example.dndn.common.model.BaseResponse;
 import org.example.dndn.document_management.model.DocumentManagementDto;
+import org.example.dndn.project.model.enums.DocType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -17,5 +19,18 @@ public class DocumentManagementController {
     public BaseResponse read(@PathVariable(value = "project_id") Long project_id){
         List<DocumentManagementDto.ReadRes> res = documentManagementService.read(project_id);
         return BaseResponse.success(res);
+    }
+
+    @PostMapping("/upload")
+    public BaseResponse upload(
+            @RequestParam("file") MultipartFile file,
+            @RequestParam("docType") DocType docType,
+            @RequestParam("docDate") String docDate,
+            @RequestParam("origin") String origin,
+            @RequestParam(value = "partnerName", required = false) String partnerName,
+            @RequestParam(value = "version", required = false) String version,
+            @RequestParam(value = "memo", required = false) String memo
+    ){
+        return BaseResponse.success("성공");
     }
 }
